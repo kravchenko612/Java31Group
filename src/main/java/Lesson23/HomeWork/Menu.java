022d7d6c024c733fde8ffec1e329005544ffaaff
krauc <krauchenkonespit@gmail.com>
2023-11-25 13:11:08 +0100

    init commit

--- src/main/java/Lesson23/HomeWork/Menu.java
+++ src/main/java/Lesson23/HomeWork/Menu.java
@@ -1,19 +1,123 @@
 package Lesson23.HomeWork;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Menu {
-      public void menu() {
+    //Завдання 2:
+    // Розробіть додаток, який дозволяє зберігати інформацію про логіни і паролі користувачів.
+    // Кожному користувачу відповідає пара "логін — пароль".
+    // При старті додатку відображається меню:
+    // 1. Додати нового користувача.
+    // 2. Видалити існуючого користувача.
+    // 3. Перевірити, чи існує такий користувач.
+    // 4. Змінити логін існуючого користувача.
+    // 5. Змінити пароль існуючого користувача.
+    // Дія виконується залежно від вибору користувача,
+    // після чого меню з'являється знову.
+    // Для вирішення завдання використовуйте відповідний клас з Java Collections Framework.
+    public void menu() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Виберіть операцію");
+        List<User> users = new ArrayList<>();
+        users.add(new User("Petrenko20", "123456P"));
+        users.add(new User("Kovalenko07", "65412uu"));
+        users.add(new User("Nesterenko55", "1568984Ql1"));
 
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("==========START==========");
-            System.out.println("=========Add new User=========");
-            System.out.println("========Delete existing User========");
-            System.out.println("=======Remark existing User=======");
-            System.out.println("======Change User name======");
-            System.out.println("=====Change User password=====");
-      }
+        while (true) {
+            System.out.println("1 Додати нового користувача.");
+            System.out.println("2 Видалити існуючого користувача.");
+            System.out.println("3 Перевірити, чи існує такий користувач.");
+            System.out.println("4 Змінити логін існуючого користувача.");
+            System.out.println("5 Змінити пароль існуючого користувача.");
+            System.out.println("6 Показати всіх користувачів.");
+            System.out.println("0 Вихід.");
 
-      public Menu() {
-      }
+
+            int choice = scanner.nextInt();
+            boolean isSelect = true;
+            switch (choice) {
+                case 1 -> add(users, scanner);
+                case 2 -> remove(users, scanner);
+                case 3 -> check(users, scanner);
+                case 4 -> changeLogin(users, scanner);
+                case 5 -> changePassword(users, scanner);
+                case 6 -> show(users);
+                case 0 -> {
+                    System.out.println("До побачення");
+                    isSelect = false;;
+                }
+                default ->  System.out.println("Помилка введення операції. Повторіть спробу. Оберіть цифри зі списку.");
+
+            }
+        }
+    }
+
+
+    public static void add(List<User> list, Scanner scanner) {
+
+        System.out.println("Додати нового користувача ");
+        System.out.print("Введіть логін: ");
+        String login = scanner.next();
+
+        System.out.print("Введіть пароль: ");
+        String password = scanner.next();
+
+        User newUser = new User(login, password);
+        list.add(newUser);
+
+    }
+
+    public static void remove(List<User> list, Scanner scanner) {
+        System.out.println("Видалити існуючого користувача.");
+        System.out.println("Введіть логін користувача, якого потрібно видатити");
+        list.removeIf(user -> user.getLogin().equals(scanner.next()));
+
+    }
+
+    public static void check(List<User> list, Scanner scanner) {
+        System.out.println("Введіть логін користувача, якого потрібно перевірити");
+        String login = scanner.next();
+
+        for (User user : list) {
+            if (user.getLogin().equals(login)) {
+                System.out.println("Такий користувач вже існує.");
+            } else {
+                System.out.println("НЕ існує.");
+            } return;
+        }
+    }
+
+    public static void changeLogin(List<User> list, Scanner scanner) {
+        System.out.println("Введіть логін користувача, який потрібно змінити");
+        String login = scanner.next();
+        System.out.println("Введіть новий логін");
+        String newLogin = scanner.next();
+        for (User user : list) {
+            if (user.getLogin().equals(login)) {
+                user.setLogin(newLogin);
+                return;
+            }
+        }
+    }
+
+    public static void changePassword(List<User> list, Scanner scanner) {
+        System.out.println("Введіть логін користувача, який потрібно змінити");
+        String login= scanner.next();
+        System.out.println("Введіть новий пароль");
+        String newPassword = scanner.next();
+        for (User user : list) {
+            if (user.getLogin().equals(login)) {
+                user.setPassword(newPassword);
+                return;
+            }
+        }
+    }
+    public static void show(List<User> list) {
+        for (User user : list) {
+            System.out.println("Логін: " + user.getLogin()+ " пароль: " + user.getPassword());
+            System.out.println();
+        }
+
+    }
 }
+
---
+++ src/main/java/Lesson23/HomeWork/User.java
@@ -0,0 +1,35 @@
+package Lesson23.HomeWork;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class User {
+    String login;
+    String password;
+
+    public User() {
+    }
+
+    public User(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
